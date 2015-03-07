29ba7d4fafc287061f1a220f7ed08fa1d34d0497
Anna <anna@Bjorns-iMac.local>
2015-03-07 11:25:52 -0500

    Added AVLTree class with Lazy Deletion

--- LazyAVLTree.h
+++ LazyAVLTree.h
@@ -1,25 +1,40 @@
+#ifndef AVL_TREE_H
+#define AVL_TREE_H
+
+#include "dsexceptions.h"
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
 /*****************************************************************************
  Title:             LazyAVLTree.h
  Author:            Anna Cristina Karingal
  Created on:        February 21, 2015
- Description:       Template class for AVL Tree that supports lazy deletion
+ Description:       Template class for an AVL Tree that supports lazy deletion
  
- Last Modified:     March 6, 2015
+ Last Modified:     March 7, 2015
  
  *****************************************************************************/
 
-#ifndef ____LazyAVLTree__
-#define ____LazyAVLTree__
-
-#include <iostream>
-#include "AvlTree.h"
-using namespace std;
-
+//
+// CONSTRUCTION: zero parameter
+//
+// ******************PUBLIC OPERATIONS*********************
+// void insert( x )       --> Insert x
+// void remove( x )       --> Remove x (unimplemented)
+// bool contains( x )     --> Return true if x is present
+// Comparable findMin( )  --> Return smallest item
+// Comparable findMax( )  --> Return largest item
+// boolean isEmpty( )     --> Return true if empty; else false
+// void makeEmpty( )      --> Remove all items
+// void printTree( )      --> Print tree in sorted order
+// ******************ERRORS********************************
+// Throws UnderflowException as warranted
 
 template <typename Comparable>
-class LazyAvlTree {
+class LazyAvlTree
+{
 public:
-    // Constructor & copy constructors
     LazyAvlTree( ) : root{ nullptr }
     { }
     
@@ -33,16 +48,90 @@ public:
         rhs.root = nullptr;
     }
     
-    // Destructor
-    ~LazyAvlTree() {
-        //AvlTree<Comparable>::makeEmpty(  );
+    ~LazyAvlTree( )
+    {
+        makeEmpty( );
     }
     
-    void remove (const Comparable &x) {
+    /**
+     * Deep copy.
+     */
+    LazyAvlTree & operator=( const LazyAvlTree & rhs )
+    {
+        LazyAvlTree copy = rhs;
+        std::swap( *this, copy );
+        return *this;
+    }
+    
+    /**
+     * Move.
+     */
+    LazyAvlTree & operator=( LazyAvlTree && rhs )
+    {
+        std::swap( root, rhs.root );
         
+        return *this;
+    }
+    
+    /**
+     * Find the smallest item in the tree.
+     * Throw UnderflowException if empty.
+     */
+    const Comparable & findMin( ) const
+    {
+        if( isEmpty( ) )
+            throw UnderflowException{ };
+        return findMin( root )->element;
+    }
+    
+    /**
+     * Find the largest item in the tree.
+     * Throw UnderflowException if empty.
+     */
+    const Comparable & findMax( ) const
+    {
+        if( isEmpty( ) )
+            throw UnderflowException{ };
+        return findMax( root )->element;
+    }
+    
+    /**
+     * Returns true if x is found in the tree.
+     */
+    bool contains( const Comparable & x ) const
+    {
         int count = 0;
-        remove( x, root, count );
-        cout << "Recursive calls to remove: " << count << endl;
+        bool c = contains( x, root, count );
+        cout << "Recursive calls to contains: " << count << endl;
+        return c;
+    }
+    
+    /**
+     * Test if the tree is logically empty.
+     * Return true if empty, false otherwise.
+     */
+    bool isEmpty( ) const
+    {
+        return root == nullptr;
+    }
+    
+    /**
+     * Print the tree contents in sorted order.
+     */
+    void printTree( ) const
+    {
+        if( isEmpty( ) )
+            cout << "Empty tree" << endl;
+        else
+            printTree( root );
+    }
+    
+    /**
+     * Make the tree logically empty.
+     */
+    void makeEmpty( )
+    {
+        makeEmpty( root );
     }
     
     /**
@@ -65,81 +154,303 @@ public:
         cout << "Recursive calls to insert: " << count << endl;
     }
     
-    bool contains( const Comparable & x ) const
+    /**
+     * Remove x from the tree. Nothing is done if x is not found.
+     */
+    void remove( const Comparable & x )
     {
         int count = 0;
-        bool c = contains( x, root, count );
-        cout << "Recursive calls to contains: " << count << endl;
-        return c;
+        remove( x, root, count );
+        cout << "Recursive calls to remove: " << count << endl;
     }
     
-    bool isEmpty( ) const
-    {
-        return root == nullptr;
-    }
-    
-    void printTree()
-    {
-            if( isEmpty( ) )
-                cout << "Empty tree" << endl;
-            else
-                printTree( root );
-        
-    }
-
 private:
-    struct LazyAvlNode {
-        
+    struct LazyAvlNode
+    {
         Comparable element;
         LazyAvlNode *left;
         LazyAvlNode *right;
         int height;
-        bool deleted;
+        bool isDeleted;
         
-        LazyAvlNode ( const Comparable &ele, LazyAvlNode *lt, LazyAvlNode *rt, int h = 0, bool del = false) : element { ele }, left{ lt }, right{ rt }, height{ h }, deleted { del } { }
+        LazyAvlNode( const Comparable & ele, LazyAvlNode *lt, LazyAvlNode *rt, int h = 0, bool del = false)
+        : element{ ele }, left{ lt }, right{ rt }, height{ h }, isDeleted{ del } { }
         
-        
-        LazyAvlNode ( Comparable &ele, LazyAvlNode *lt, LazyAvlNode *rt, int h = 0, bool del = false) : element { std::move( ele ) }, left{ lt }, right{ rt }, height{ h }, deleted { del } { }
-        
+        LazyAvlNode( Comparable && ele, LazyAvlNode *lt, LazyAvlNode *rt, int h = 0, bool del = false )
+        : element{ std::move( ele ) }, left{ lt }, right{ rt }, height{ h }, isDeleted{ del } { }
     };
     
     LazyAvlNode *root;
     
-    static const int ALLOWED_IMBALANCE = 1;
     
-    // Insert functions
+    /**
+     * Internal method to insert into a subtree.
+     * x is the item to insert.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void insert( const Comparable & x, LazyAvlNode * & t, int &count )
+    {
+        if( t == nullptr )
+            t = new LazyAvlNode{ x, nullptr, nullptr };
+        else if( x < t->element ){
+            count ++;
+            insert( x, t->left, count);
+        }
+        else if( t->element < x ){
+            count ++;
+            insert( x, t->right, count);
+        }
+        else {
+            ; // Duplicate
+        }
+        
+        balance( t );
+    }
     
-    void insert(const Comparable &x, LazyAvlNode *&t, int &count);
+    /**
+     * Internal method to insert into a subtree.
+     * x is the item to insert.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void insert( Comparable && x, LazyAvlNode * & t, int &count)
+    {
+        if( t == nullptr )
+            t = new LazyAvlNode{ std::move( x ), nullptr, nullptr };
+        else if( x < t->element ){
+            count ++;
+            insert( std::move( x ), t->left, count );
+        }
+        else if( t->element < x ){
+            count ++;
+            insert( std::move( x ), t->right, count);
+        }
+        
+        balance( t );
+    }
     
-    void insert (Comparable &&x, LazyAvlNode *&t, int &count);
-    
-    // Find functions
-    
-    LazyAvlNode * find ( const Comparable & x, LazyAvlNode *t, int &count );
-    
-    bool contains ( const Comparable & x, LazyAvlNode *t, int &count ) const;
-    
-    // Remove functions
-    
-    void remove (const Comparable &x, LazyAvlNode *&t, int &count);
-    
-    // Balance functions
-    
-    void balance (LazyAvlNode *&t);
-    
-    void rotateWithLeftChild( LazyAvlNode * & k2 );
-    
-    void rotateWithRightChild( LazyAvlNode * & k1 );
-    
-    void doubleWithLeftChild( LazyAvlNode * & k3 );
-    
-    void doubleWithRightChild( LazyAvlNode * & k1 );
+    /**
+     * Internal method to remove find a node containing the Comparable element in the subtree
+     * x is the item to find.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     * Returns a pointer to the node containing the element
+     * If tree does not contain element, returns nullptr
+     */
+    LazyAvlNode* find ( const Comparable & x, LazyAvlNode * t, int &count){
+        
+        if (t == nullptr){
+            return nullptr;
+        }
+        else if( x < t->element ){
+            count ++;
+            return find( x, t->left, count );
+        }
+        else if( t->element < x ){
+            count++;
+            return find( x, t->right, count );
+        }
+        else{ // Found a match, return pointer to node
+            if (!t->deleted) {
+                return *t;
+            }
+            else {
+                return nullptr;
+            }
+        }
+
+    }
 
     
-    // Output functions
+    /**
+     * Internal method to remove from a subtree.
+     * x is the item to remove.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void remove( const Comparable & x, LazyAvlNode * & t, int &count)
+    {
+        // Find element containing x
+        LazyAvlNode* found = find(x, t, count);
+        if (found == nullptr){
+            // Do nothing. No node to remove
+        }
+        else { // Mark node as deleted
+            found->deleted = true;
+        }
+    }
     
-    void printTree (LazyAvlNode *t) const;    
+    static const int ALLOWED_IMBALANCE = 1;
     
+    // Assume t is balanced or within one of being balanced
+    void balance( LazyAvlNode * & t )
+    {
+        if( t == nullptr )
+            return;
+        
+        if( height( t->left ) - height( t->right ) > ALLOWED_IMBALANCE )
+            if( height( t->left->left ) >= height( t->left->right ) )
+                rotateWithLeftChild( t );
+            else
+                doubleWithLeftChild( t );
+            else
+                if( height( t->right ) - height( t->left ) > ALLOWED_IMBALANCE )
+                    if( height( t->right->right ) >= height( t->right->left ) )
+                        rotateWithRightChild( t );
+                    else
+                        doubleWithRightChild( t );
+        
+        t->height = max( height( t->left ), height( t->right ) ) + 1;
+    }
+    
+    /**
+     * Internal method to find the smallest item in a subtree t.
+     * Return node containing the smallest item.
+     */
+    LazyAvlNode * findMin( LazyAvlNode *t ) const
+    {
+        if( t == nullptr )
+            return nullptr;
+        if( t->left == nullptr )
+            return t;
+        return findMin( t->left );
+    }
+    
+    /**
+     * Internal method to find the largest item in a subtree t.
+     * Return node containing the largest item.
+     */
+    LazyAvlNode * findMax( LazyAvlNode *t ) const
+    {
+        if( t != nullptr )
+            while( t->right != nullptr )
+                t = t->right;
+        return t;
+    }
+    
+    
+    /**
+     * Internal method to test if an item is in a subtree.
+     * x is item to search for.
+     * t is the node that roots the tree.
+     */
+    bool contains( const Comparable & x, LazyAvlNode *t, int &count ) const
+    {
+        LazyAvlNode* found = find(x, t, count);
+        if (found != nullptr){
+            return true;
+        }
+        else { // Node not found or is marked as deleted
+            return false;
+        }
+    }
+    
+    /**
+     * Internal method to make subtree empty.
+     */
+    void makeEmpty( LazyAvlNode * & t )
+    {
+        if( t != nullptr )
+        {
+            makeEmpty( t->left );
+            makeEmpty( t->right );
+            delete t;
+        }
+        t = nullptr;
+    }
+    
+    /**
+     * Internal method to print a subtree rooted at t in sorted order.
+     */
+    void printTree( LazyAvlNode *t ) const
+    {
+        // Print to console if tree node is not null and is not marked as deleted
+        if( t != nullptr && !t->isDeleted)
+        {
+            printTree( t->left );
+            cout << t->element << endl;
+            printTree( t->right );
+        }
+    }
+    
+    /**
+     * Internal method to clone subtree.
+     */
+    LazyAvlNode * clone( LazyAvlNode *t ) const
+    {
+        if( t == nullptr )
+            return nullptr;
+        else
+            return new LazyAvlNode{ t->element, clone( t->left ), clone( t->right ), t->height };
+    }
+    // Avl manipulations
+    /**
+     * Return the height of node t or -1 if nullptr.
+     */
+    int height( LazyAvlNode *t ) const
+    {
+        return t == nullptr ? -1 : t->height;
+    }
+    
+    int max( int lhs, int rhs ) const
+    {
+        return lhs > rhs ? lhs : rhs;
+    }
+    
+    /**
+     * Rotate binary tree node with left child.
+     * For AVL trees, this is a single rotation for case 1.
+     * Update heights, then set new root.
+     */
+    void rotateWithLeftChild( LazyAvlNode * & k2 )
+    {
+        LazyAvlNode *k1 = k2->left;
+        k2->left = k1->right;
+        k1->right = k2;
+        k2->height = max( height( k2->left ), height( k2->right ) ) + 1;
+        k1->height = max( height( k1->left ), k2->height ) + 1;
+        k2 = k1;
+    }
+    
+    /**
+     * Rotate binary tree node with right child.
+     * For AVL trees, this is a single rotation for case 4.
+     * Update heights, then set new root.
+     */
+    void rotateWithRightChild( LazyAvlNode * & k1 )
+    {
+        LazyAvlNode *k2 = k1->right;
+        k1->right = k2->left;
+        k2->left = k1;
+        k1->height = max( height( k1->left ), height( k1->right ) ) + 1;
+        k2->height = max( height( k2->right ), k1->height ) + 1;
+        k1 = k2;
+    }
+    
+    /**
+     * Double rotate binary tree node: first left child.
+     * with its right child; then node k3 with new left child.
+     * For AVL trees, this is a double rotation for case 2.
+     * Update heights, then set new root.
+     */
+    void doubleWithLeftChild( LazyAvlNode * & k3 )
+    {
+        rotateWithRightChild( k3->left );
+        rotateWithLeftChild( k3 );
+    }
+    
+    /**
+     * Double rotate binary tree node: first right child.
+     * with its left child; then node k1 with new right child.
+     * For AVL trees, this is a double rotation for case 3.
+     * Update heights, then set new root.
+     */
+    void doubleWithRightChild( LazyAvlNode * & k1 )
+    {
+        rotateWithLeftChild( k1->right );
+        rotateWithRightChild( k1 );
+    }
 };
 
 #endif
