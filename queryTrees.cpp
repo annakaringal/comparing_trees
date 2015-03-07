01323cd13a6ed77971d5e49f636224e9bf4d37fb
Anna <anna@Bjorns-iMac.local>
2015-03-07 14:13:25 -0500

    Created queryTrees

--- queryTrees.cpp
+++ queryTrees.cpp
@@ -15,58 +15,12 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    vector<int> myints = {1,9,10,3,5,4,6,8,7,2};
-    
-    LazyAvlTree<int> lazy;
-    
-    for (auto x: myints){
-        lazy.insert(x);
-    }
-    /*
-    AvlTree<int> avl;
-    cout << "====" << endl;
-    
-    for (auto x: myints) {
-        avl.insert(x);
-    }*/
-    
-    cout << "Min: " << lazy.findMin() << endl;
-    cout << "Max: " << lazy.findMax() << endl;
-    vector<int> removeints = {1,10};
-    
-    for (auto x: removeints) {
-        cout << "===" << endl;
-        cout << "Lazy removing " << x << "..." << endl;
-        lazy.remove(x);
-        
-    }
-    cout << "Min: " << lazy.findMin() << endl;
-    cout << "Max: " << lazy.findMax() << endl;
-    
-    lazy.remove(2);
-    lazy.remove(9);
-    
-    cout << "Min: " << lazy.findMin() << endl;
-    cout << "Max: " << lazy.findMax() << endl;
-    
-    /*
-    
-    
-    for (auto x: removeints) {
-        cout << "===" << endl;
-        cout << "AVL Removing " << x << "..." << endl;
-       avl.remove(x);
-        
-    }*/
-    
-    /*
     if (argc !=3){
         // Incorrect number of arguments given in command line
         cerr << "ERROR: Invalid number of arguments." << endl;
         exit(-1);
     }
     else {
-        
         
         string file_name = argv[1];
         string tree_type = argv[2];
@@ -82,16 +36,28 @@ int main(int argc, const char * argv[]) {
         if (readf.is_open()) {
             
             try {
-                // Parse tree
-                BinarySearchTree<SequenceMap> bst = parse_tree<BinarySearchTree<SequenceMap>>(readf);
+                
+                if (tree_type == "BST") {
+                    BinarySearchTree<SequenceMap> tree = parse_tree<BinarySearchTree<SequenceMap>>(readf);
+                }
+                else if (tree_type == "AVL"){
+                    AvlTree<SequenceMap> tree = parse_tree<AvlTree<SequenceMap>>(readf);
+                }
+                else if (tree_type == "LazyAVL") {
+                    LazyAvlTree<SequenceMap> tree = parse_tree<LazyAvlTree<SequenceMap>>(readf);
+                }
+                else {
+                    throw invalid_argument(tree_type);
+                }
                 
                 // Get user query
                 cout << "Recognition sequence: ";
                 string query_seq;
                 cin >> query_seq;
                 
-                
                 // If sequence is in tree, print all corresponding enzymes
+                
+                
                 
             }
             catch (...) {
@@ -102,8 +68,7 @@ int main(int argc, const char * argv[]) {
         
         readf.close();
         
-    }*/
-    
+    }
     
     return 0;
 }
