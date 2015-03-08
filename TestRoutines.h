3b33a1a2b9a29d3849b993541b21abf3c70403e6
Anna <anna@Bjorns-iMac.local>
2015-03-08 15:52:58 -0400

    Added function to calculate avg depth ratio

--- TestRoutines.h
+++ TestRoutines.h
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 
 #include "SequenceMap.h"
 
@@ -14,19 +15,31 @@ using namespace std;
 template <typename TreeType>
 void get_tree_characteristics(TreeType &tree) {
     
-    
-    // Calculate and print number of nodes in tree
-    int n = tree.num_of_nodes();
-    cout << "Number of Nodes: " << n << endl;
-    
-    // Compute and print average depth of tree
-    float avg_depth = tree.internal_path_length() / n;
-    cout << "Average Depth: " << setprecision(2) << avg_depth << endl;
-    
-    // Computes and prints ratio of avg depth to log n
-    float ratio;
-    
-    
+    if (!tree.isEmpty()) {
+        
+        // Calculate and print number of nodes in tree
+        int n = tree.num_of_nodes();
+        cout << "Number of Nodes: " << n << endl;
+        
+        // Compute and print average depth of tree
+        float avg_depth = tree.internal_path_length() / n;
+        cout << "Average Depth: " << setprecision(2) << avg_depth << endl;
+        
+        if (n > 1) {
+            // Computes and prints ratio of avg depth to log n
+            cout << "Ratio of Avg Depth: " << setprecision(3) << avg_depth/log2(n) << endl;
+        }
+        else {
+            // Only 1 node
+            cout << "Ratio of Avg Depth: 0" << endl;
+        }
+
+    }
+    else {
+        
+        cout << "Empty tree. No information to calculate." << endl;
+        
+    }
     
 }
 
